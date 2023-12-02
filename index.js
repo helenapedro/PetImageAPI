31a51b579c3f083bcaf2e27e2c3dd84da6fc57e6
Helena Pedro <mbeua94@gmail.com>
2023-12-02 07:35:45 +0100

    Some updates

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const apiRoutes = require();
+const apiRoutes = require('./routes/apiRoutes');
 const errorHandler = require('./middleware/errorMiddleware');
+const dotenv = require('dotenv');
 
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
-const SECRET_KEY = 
 
 app.use('/api', apiRoutes);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-     console.log('Listening on port ${PORT}');
+     console.log(`Listening on port ${PORT}`);
 });
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const SECRET_KEY ='';
+const SECRET_KEY = process.env.SECRET_KEY ||'main_secret_key';
 
 function authenticate(req, res, next) {
      const token = req.headers.authorization;
