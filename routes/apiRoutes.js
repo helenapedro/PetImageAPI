0433949051589c5bb0ddc332e376ff338fc444a5
Helena Pedro <mbeua94@gmail.com>
2023-12-02 13:25:36 +0100

    update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,41 +6,41 @@ const router = express.Router();
 const usedDogImages = [];
 const usedCatImages = [];
 
-// Endpoint para obter uma foto aleatória de um cachorro
+// Endpoint to get a random image of a cat 
 router.get('/getdog', async (req, res) => {
   try {
     const response = await axios.get('https://dog.ceo/api/breeds/image/random');
     const imageUrl = response.data.message;
 
-    // Verifica se a imagem já foi usada nesta sessão
+    // Verifies if the image was used in this session 
     if (usedDogImages.includes(imageUrl)) {
-      return res.status(500).json({ error: 'Imagem de cachorro repetida nesta sessão' });
+      return res.status(500).json({ error: 'Dog image repeated in this session' });
     }
 
     usedDogImages.push(imageUrl);
     res.send(`<img src="${imageUrl}"/>`);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Erro ao obter imagem de cachorro' });
+    res.status(500).json({ error: 'Error getting dog image' });
   }
 });
 
-// Endpoint para obter uma foto aleatória de um gato
+// Endpoint to get a random image of a cat
 router.get('/getcat', async (req, res) => {
   try {
     const response = await axios.get('https://api.thecatapi.com/v1/images/search');
     const imageUrl = response.data[0].url;
 
-    // Verifica se a imagem já foi usada nesta sessão
+    // Verifies if the image was used in this session 
     if (usedCatImages.includes(imageUrl)) {
-      return res.status(500).json({ error: 'Imagem de gato repetida nesta sessão' });
+      return res.status(500).json({ error: 'Cat image repeated in this session' });
     }
 
     usedCatImages.push(imageUrl);
     res.send(`<img src="${imageUrl}"/>`);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Error to get the Cat Image' });
+    res.status(500).json({ error: 'Error getting cat image' });
   }
 });
 
