16639196fd646df41e5e958966abeb49aa12ab7e
Helena Pedro <mbeua94@gmail.com>
2023-12-02 07:21:37 +0100

    Set Routes API

---
+++ routes/apiRoutes.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const axios = require('axios');
+const authenticate = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+const usedDogImages = [];
+const usedCatImages = [];
+
+// Endpoint para obter uma foto aleatória de um cachorro
+router.get('/getdog', authenticate, async (req, res) => {
+  try {
+    const response = await axios.get('https://dog.ceo/api/breeds/image/random');
+    const imageUrl = response.data.message;
+
+    // Verifica se a imagem já foi usada nesta sessão
+    if (usedDogImages.includes(imageUrl)) {
+      return res.status(500).json({ error: 'Imagem de cachorro repetida nesta sessão' });
+    }
+
+    usedDogImages.push(imageUrl);
+    res.json({ imageUrl });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao obter imagem de cachorro' });
+  }
+});
+
+// Endpoint para obter uma foto aleatória de um gato
+router.get('/getcat', authenticate, async (req, res) => {
+  try {
+    const response = await axios.get('https://api.thecatapi.com/v1/images/search');
+    const imageUrl = response.data[0].url;
+
+    // Verifica se a imagem já foi usada nesta sessão
+    if (usedCatImages.includes(imageUrl)) {
+      return res.status(500).json({ error: 'Imagem de gato repetida nesta sessão' });
+    }
+
+    usedCatImages.push(imageUrl);
+    res.json({ imageUrl });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error to get the Cat Image' });
+  }
+});
+
+module.exports = router;
